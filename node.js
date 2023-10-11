4c2b77920f42338e1561e8a9e4a3f0fc531dcd8d
MatteoPas93 <pascucci-matteo@outlook.it>
2023-10-11 18:32:17 +0200

    Create node.js

---
+++ node.js
@@ -0,0 +1,23 @@
+// Domanda su cos'è un algoritmo.
+let primaDomanda = "Cos'è un algoritmo?";
+console.log(primaDomanda)
+
+// Risposta ed esempio di algoritmo.
+let primaRisposta = "- Un algoritmo è un insieme di fasi prestabilite utilizzate per risolvere problemi seguendo dei passaggi precisi. Un esempio è quello di paragonare un algoritmo al montaggio di un mobile/scaffale. I dati iniziali rappresentano i componenti a nostra disposizione, le istruzioni indicano come combinare tra loro i vari componenti e il risultato finale, in questo caso, è il/lo mobile/scaffale finito.";
+console.log(primaRisposta)
+
+// Domanda della variabile.
+let secondaDomanda = "Cos'è una variabile?";
+console.log(secondaDomanda);
+
+// Risposta ed esmpio di variabile.
+let secondaRisposta = "- Una variabile può essere vista come un contenitore con un nome (unico), utilizzato per contenere i risultati parziali dell'algoritmo. "
+console.log(secondaRisposta)
+
+// Domanda undefined e null.
+let terzaDomanda = "Undefined e Null sono la stessa cosa?"
+console.log(terzaDomanda)
+
+// Risposta undefined e null.
+let terzaRisposta = "- Undefined è un errore, indica che una variabile non è stata dichiarata. Null invece è utilizzato per indicare che una variabile non fa riferimento a nessun oggetto o valore specifico."
+console.log(terzaRisposta)
